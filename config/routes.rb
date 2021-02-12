3f691bbb622b6d3f0bee1d8efe2eb095bc5231a5
Scott Fleming <sdflem@gmail.com>
2021-02-12 16:52:08 -0600

    Complete playlist-maker app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: redirect('/tracks')
+
+  get 'tracks', to: 'tracks#index', as: 'tracks'
+  get 'tracks/:id', to: 'tracks#show', as: 'track'
+
 end
