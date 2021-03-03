53e3fe07c6cfe0bf8157cbf3698e9884f20220ee
Scott Fleming <sdflem@gmail.com>
2021-03-03 14:46:36 -0600

    Fix a couple bugs in freebay code

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
 
   root to: redirect('/listings')
 
-  resources :listings
+  get 'listings', to: 'listings#index', as: 'listings'
+  post 'listings', to: 'listings#create'
+  get 'listings/new', to: 'listings#new', as: 'new_listing'
+  get 'listings/:id', to: 'listings#show', as: 'listing'
+  patch 'listings/:id', to: 'listings#update'
+  put 'listings/:id', to: 'listings#update'
+  delete 'listings/:id', to: 'listings#destroy'
+  get 'listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
+
 end
